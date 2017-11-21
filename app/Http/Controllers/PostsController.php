50472ad173dbd3d7d4ff1036c51d7a5449ee82eb
陳美妤 <s3a432090@student.ncut.edu.tw>
2017-11-21 21:04:08 +0800

    5. 設定 Controller 回傳對應的 view

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    public function index()
+    {
+        //
+        return view('index');
+    }
+    public function show()
+    {
+        //
+        return view('post');
+    }
+    public function about()
+    {
+        //
+        return view('about');
+    }
+    public function contact()
+    {
+        //
+        return view('contact');
+    }
 }
